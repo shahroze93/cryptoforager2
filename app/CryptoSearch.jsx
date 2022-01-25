fbe0e1efe182c9306b6c443821108543efdffc82
Shahroze Husain <shahroze93@gmail.com>
2022-01-25 19:52:32 +0800

    added inital code for search

---
+++ app/CryptoSearch.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class CryptoSearch extends Component {
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+export default CryptoSearch;
